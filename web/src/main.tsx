ede9b6fe8eedae07caff99947265c2c3d8cec8e0
Guilherme Martins <g.santosmartins@outlook.com>
2022-05-06 20:18:18 -0300

    feat: Adding Tailwind Css and postCss

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './index.css'
+
+// importing global css, vite deals with it
+import './global.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
+  // strict mode activates the strict js mode on app
   <React.StrictMode>
     <App />
   </React.StrictMode>
---
+++ web/tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  content: ["./src/**/*.tsx"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+ 
+// make the install with the post to be watching the new css changes
